38d0f1584f38a4770ef6a9f824d53be524901e15
Mirek Rusin <mirek@me.com>
2019-03-07 16:45:08 +0100

    Adding address of public key helper.

---
+++ address-of-public-key.js
@@ -0,0 +1,17 @@
+// @flow
+
+const { inspect } = require('util')
+const { isBuffer } = Buffer
+const keccak256OfBuffer = require('./keccak256-of-buffer')
+
+function addressOfPublicKey(publicKey /*: Buffer */) /*: Buffer */ {
+  if (!isBuffer(publicKey)) {
+    throw new TypeError(`Expected public key buffer, got ${inspect(publicKey)}.`)
+  }
+  if (publicKey.length !== 64) {
+    throw new TypeError(`Expected public key buffer of 64 bytes, got ${inspect(publicKey.length)}.`)
+  }
+  return keccak256OfBuffer(publicKey).slice(0, 20)
+}
+
+module.exports = addressOfPublicKey
