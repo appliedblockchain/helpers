f10b546261a10330d097c662f4b2e1967d3ae230
Mirek Rusin <mirek@me.com>
2019-03-07 16:44:54 +0100

    Adding scrypt-of helper.

---
+++ scrypt-of.js
@@ -0,0 +1,53 @@
+// @flow
+
+const { inspect } = require('util')
+const { randomBytes, scrypt } = require('crypto')
+const { isBuffer } = Buffer
+
+const defaultN = process.env.NODE_ENV === 'test' ? 2 : 16384
+const defaultDklen = 32
+const defaultR = 8
+const defaultP = 1
+const defaultMemory = 32 * 1024 * 1024
+
+if (128 * defaultN * defaultR > defaultMemory) {
+  throw new Error(`Invalid scrypt defaults ${inspect({ defaultN, defaultR, defaultMemory })}.`)
+}
+
+function scryptOf(
+  password /*: string */,
+  { n = defaultN, dklen = defaultDklen, r = defaultR, p = defaultP, memory = defaultMemory, salt } /*: {|
+    n?: number,
+    dklen?: number,
+    r?: number,
+    p?: number,
+    maxmem?: number,
+    salt?: Buffer
+  |} */ = {}
+) /*: Promise<{|
+  n: number,
+  dklen: number,
+  r: number,
+  p: number,
+  salt: Buffer,
+  derivedKey: Buffer
+|}> */ {
+  const effectiveSalt = salt || randomBytes(16)
+  if (!isBuffer(effectiveSalt)) {
+    throw new TypeError(`Expected salt to be a buffer, got ${typeof effectiveSalt}.`)
+  }
+  if (effectiveSalt.length !== 16) {
+    throw new TypeError(`Expected salt length to be 16, got ${effectiveSalt.length}.`)
+  }
+  return new Promise((resolve, reject) => {
+    scrypt(password, effectiveSalt, dklen, { N: n, r, p, maxmem: memory }, (err, derivedKey) => {
+      if (err) {
+        reject(err)
+      } else {
+        resolve({ n, memory, dklen, r, p, salt: effectiveSalt, derivedKey })
+      }
+    })
+  })
+}
+
+module.exports = scryptOf
