40e3069c169fe94c6d0618b2d1da8b4da6bf1754
Mirek Rusin <mirek@me.com>
2019-03-08 09:05:21 +0100

    Fixing address of public key (last 20 bytes, not first 20 bytes).

--- address-of-public-key.js
+++ address-of-public-key.js
@@ -11,7 +11,7 @@ function addressOfPublicKey(publicKey /*: Buffer */) /*: Buffer */ {
   if (publicKey.length !== 64) {
     throw new TypeError(`Expected public key buffer of 64 bytes, got ${inspect(publicKey.length)}.`)
   }
-  return keccak256OfBuffer(publicKey).slice(0, 20)
+  return keccak256OfBuffer(publicKey).slice(-20)
 }
 
 module.exports = addressOfPublicKey
