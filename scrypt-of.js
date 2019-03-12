d10309165307b53578a77f9f0285afb7f495596a
Mirek Rusin <mirek@me.com>
2019-03-12 13:32:16 +0100

    Updating scrypt key derivation function, dropping memory output.

--- scrypt-of.js
+++ scrypt-of.js
@@ -9,24 +9,17 @@ const defaultN = process.env.NODE_ENV === 'test' ? 2 : 16384
 const defaultDklen = 32
 const defaultR = 8
 const defaultP = 1
-const defaultMemory = 32 * 1024 * 1024
-
-if (128 * defaultN * defaultR > defaultMemory) {
-  throw new Error(`Invalid scrypt defaults ${inspect({ defaultN, defaultR, defaultMemory })}.`)
-}
 
 function scryptOf(
   password /*: string */,
-  { n = defaultN, dklen = defaultDklen, r = defaultR, p = defaultP, memory = defaultMemory, salt } /*: {|
+  { n = defaultN, dklen = defaultDklen, r = defaultR, p = defaultP, salt } /*: {|
     n?: number,
     dklen?: number,
     r?: number,
     p?: number,
-    memory?: number,
     salt?: Buffer
   |} */ = {}
 ) /*: Promise<{|
-  memory: number,
   n: number,
   dklen: number,
   r: number,
@@ -34,15 +27,15 @@ function scryptOf(
   salt: Buffer,
   derivedKey: Buffer
 |}> */ {
-  const effectiveSalt = salt || randomBytes(16)
+  const effectiveSalt = salt || randomBytes(32)
   if (!isBuffer(effectiveSalt)) {
     throw new TypeError(`Expected salt to be a buffer, got ${typeof effectiveSalt}.`)
   }
-  if (effectiveSalt.length !== 16) {
-    throw new TypeError(`Expected salt length to be 16, got ${effectiveSalt.length}.`)
+  if (effectiveSalt.length < 16) {
+    throw new TypeError(`Expected salt length to be at least 16 bytes, got ${effectiveSalt.length}.`)
   }
   return new Promise((resolve, reject) => {
-    scrypt(password, effectiveSalt, dklen, { N: n, r, p, maxmem: memory }, (err, derivedKey) => {
+    scrypt(password, effectiveSalt, dklen, { N: n, r, p }, (err, derivedKey) => {
       if (err) {
         reject(err)
       } else {
@@ -50,7 +43,7 @@ function scryptOf(
           reject(new TypeError('Unexpected result, err and derived key are both null.'))
           return
         }
-        resolve({ n, memory, dklen, r, p, salt: effectiveSalt, derivedKey })
+        resolve({ n, dklen, r, p, salt: effectiveSalt, derivedKey })
       }
     })
   })
