be6a37631b69d96c4ba70b8962a851d37904b12f
Mirek Rusin <mirek@me.com>
2019-03-12 15:01:58 +0100

    Fixing uuid generator.

---
+++ uuid-string-of-buffer.js
@@ -0,0 +1,25 @@
+// @flow
+
+const { inspect } = require('util')
+const { isBuffer } = Buffer
+
+const h = []
+
+for (let i = 0; i < 0xff; ++i) {
+  h[i] = i.toString(16).padStart(2, '0')
+}
+
+function uuidStringOfBuffer(v /*: Buffer */) /*: string */ {
+  if (!isBuffer(v)) {
+    throw new TypeError(`Expected buffer, got ${inspect(v)}.`)
+  }
+  return [
+    h[v[0]], h[v[1]], h[v[2]], h[v[3]], '-',
+    h[v[4]], h[v[5]], '-',
+    h[v[6]], h[v[7]], '-',
+    h[v[8]], h[v[9]], '-',
+    h[v[10]], h[v[11]], h[v[12]], h[v[13]], h[v[14]], h[v[15]]
+  ].join('')
+}
+
+module.exports = uuidStringOfBuffer
--- uuid-v4.js
+++ uuid-v4.js
@@ -1,24 +1,11 @@
 // @flow
 
 const { randomBytes } = require('crypto')
+const { mask, variantRfc4122 } = require('./uuid')
+const uuidStringOfBuffer = require('./uuid-string-of-buffer')
 
-const h = []
-
-for (let i = 0; i < 0xff; ++i) {
-  h[i] = i.toString(16).padStart(2, '0')
-}
-
-function uuidV4() {
-  const x = randomBytes(16)
-  x[6] = (x[6] & 0x0f) | 0x40
-  x[8] = (x[8] & 0x3f) | 0x80
-  return [
-    h[x[0]], h[x[1]], h[x[2]], h[x[3]], '-',
-    h[x[4]], h[x[5]], '-',
-    h[x[6]], h[x[7]], '-',
-    h[x[8]], h[x[9]], '-',
-    h[x[10]], h[x[11]], h[x[12]], h[x[13]], h[x[14]], h[x[15]]
-  ].join('')
+function uuidV4() /*: string */ {
+  return uuidStringOfBuffer(mask(randomBytes(16), 4, variantRfc4122))
 }
 
 module.exports = uuidV4
