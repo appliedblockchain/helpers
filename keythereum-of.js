8f226a9c660b6c1831efab92b66e6eac98768693
Mirek Rusin <mirek@me.com>
2019-03-12 13:32:45 +0100

    Adding support for both pbkdf2 and scrypt in keythereum helper.

--- keythereum-of.js
+++ keythereum-of.js
@@ -1,5 +1,6 @@
 // @flow
 
+const { inspect } = require('util')
 const { randomBytes, createCipheriv } = require('crypto')
 const uuidV4 = require('./uuid-v4')
 const hexOfBuffer = require('./hex-of-buffer')
@@ -7,27 +8,45 @@ const hexOfHex0x = require('./hex-of-hex0x')
 const publicKeyOfPrivateKey = require('./public-key-of-private-key')
 const addressOfPublicKey = require('./address-of-public-key')
 const scryptOf = require('./scrypt-of')
+const pbkdf2Of = require('./pbkdf2-of')
 const keccak256OfBuffer = require('./keccak256-of-buffer')
+const { isBuffer } = Buffer
 
 const version = 3
 
 const cipher = 'aes-128-ctr'
-const kdf = 'scrypt'
+const defaultKdf = 'scrypt'
 
 async function keythereumOf(
-  { privateKey = randomBytes(32), password } /*: {|
+  { privateKey = randomBytes(32), password, kdf = defaultKdf, iv = randomBytes(16), salt = randomBytes(32) } /*: {|
     privateKey?: Buffer,
-    password: string
+    password: string,
+    kdf?: 'scrypt' | 'pbkdf2',
+    iv?: Buffer,
+    salt?: Buffer
   |}*/
 ) {
+
+  if (kdf !== 'scrypt' && kdf !== 'pbkdf2') {
+    throw new TypeError(`Expected kdf 'scrypt' or 'pbkdf2', got ${inspect(kdf)}.`)
+  }
+
+  if (!isBuffer(iv) || iv.length !== 16) {
+    throw new TypeError(`Expected iv buffer of 16 bytes, got ${inspect(iv)}.`)
+  }
+
+  if (!isBuffer(salt) || salt.length < 16) {
+    throw new TypeError(`Expected salt buffer of at least 16 bytes, got ${inspect(salt)}.`)
+  }
 
   const id = uuidV4()
 
   const publicKey = publicKeyOfPrivateKey(privateKey)
   const address = addressOfPublicKey(publicKey)
-  const { derivedKey, memory, dklen, n, r, p, salt } = await scryptOf(password)
+  const { derivedKey, dklen, salt: kdfSalt, ...kdfRest } = kdf === 'scrypt' ?
+    await scryptOf(password, { salt }) :
+    await pbkdf2Of(password, { salt })
 
-  const iv = randomBytes(16)
   const cipher_ = createCipheriv(cipher, derivedKey.slice(0, 16), iv)
   const ciphertext = Buffer.concat([
     cipher_.update(privateKey),
@@ -52,12 +71,9 @@ async function keythereumOf(
       mac: hexOfBuffer(mac),
       kdf,
       kdfparams: {
-        memory,
         dklen,
-        n,
-        r,
-        p,
-        salt: hexOfBuffer(salt)
+        salt: hexOfBuffer(kdfSalt),
+        ...kdfRest
       }
     }
   }
