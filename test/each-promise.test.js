ed5d66dbcb3f39b1b43a939be146462979dd781b
Mirek Rusin <mirek@me.com>
2019-04-03 11:21:15 +0200

    Adding timing test for each-promise helper test.

--- test/each-promise.test.js
+++ test/each-promise.test.js
@@ -5,9 +5,13 @@ const eachPromise = require('../each-promise')
 
 test('eachPromise', async () => {
 
+  const start = Date.now() / 1000
+  const timings = []
+
   const results = []
-  for await (const [ err, result, i ] of eachPromise([ sleep(3).then(() => 'a'), sleep(1).then(() => 'b'), sleep(2).then(() => 'c') ])) {
+  for await (const [ err, result, i ] of eachPromise([ sleep(3 * 1000).then(() => 'a'), sleep(1 * 1000).then(() => 'b'), sleep(2 * 1000).then(() => 'c') ])) {
     results.push([ err, result, i ])
+    timings.push((Date.now() / 1000) - start)
   }
 
   expect(results).toEqual([
@@ -15,5 +19,9 @@ test('eachPromise', async () => {
     [ void 0, 'c', 2 ],
     [ void 0, 'a', 0 ]
   ])
+
+  expect(timings[0]).toBeCloseTo(1, 1)
+  expect(timings[1]).toBeCloseTo(2, 1)
+  expect(timings[2]).toBeCloseTo(3, 1)
 
 })
