594327cba24b59e4d80e9e7ca0a7cb3abd10b7d8
Mirek Rusin <mirek@me.com>
2019-04-09 15:31:07 +0200

    Adding optional timeout to height-of.

--- height-of.js
+++ height-of.js
@@ -8,10 +8,10 @@ const numberOfHex0x = require('./number-of-hex0x')
 const defaultTiemout = 1 * 1000
 
 /** @returns Height of the chain from provided ethereum jsonrpc endpoint `url` or `NaN` if timeout has been reached or any other problem occured. */
-async function heightOf(url /*: string */) /*: Promise<number> */ {
+async function heightOf(url /*: string */, timeout /*:: ?: number */ = defaultTiemout) /*: Promise<number> */ {
 
   // TODO: Use built in timeout, otherwise we can create too many connections to failing node.
-  return rejectTimeout(postJsonRpc(url, 'eth_blockNumber'), defaultTiemout)
+  return rejectTimeout(postJsonRpc(url, 'eth_blockNumber'), timeout)
     .then(numberOfHex0x)
     .catch(() => NaN)
 }
