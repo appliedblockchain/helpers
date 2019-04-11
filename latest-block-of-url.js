0f1514a7f04bffbbbe8b89633fa8993d82bb3638
Mirek Rusin <mirek@me.com>
2019-04-11 06:48:42 +0200

    Adding latest-block-of-url helper.

---
+++ latest-block-of-url.js
@@ -0,0 +1,27 @@
+// @flow
+
+const postJsonRpc = require('./post-json-rpc')
+const rejectTimeout = require('./reject-timeout')
+const numberOfHex0x = require('./number-of-hex0x')
+const logOf = require('./log-of')
+
+const log = logOf('latest-block-of-url')
+
+const defaultIncludeTransactions = false
+
+// Default timeout (1s).
+const defaultTiemout = 1 * 1000
+
+async function latestBlockOfUrl(
+  url /*: string */,
+  includeTransactions /*:: ?: boolean */ = defaultIncludeTransactions,
+  timeout /*:: ?: number */ = defaultTiemout
+) /*: Promise<?Object> */ {
+  return rejectTimeout(postJsonRpc(url, 'eth_getBlockByNumber', 'latest', includeTransactions), timeout)
+    .catch((err /*: Error */) => {
+      log.debug('Error while trying to get latest block of url', url, err)
+      return null
+    })
+}
+
+module.exports = latestBlockOfUrl
