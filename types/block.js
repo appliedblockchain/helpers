e55d8c0f66fa4ff27fe2b59bca763de4cdfffa93
Mirek Rusin <mirek@me.com>
2019-04-12 07:51:11 +0200

    Adding block type.

---
+++ types/block.js
@@ -0,0 +1,28 @@
+// @flow
+
+/*::
+
+import type { Transaction } from './transaction'
+
+export type Block = {|
+  number: string,
+  hash: string,
+  parentHash: string,
+  nonce: string,
+  sha3Uncles: string,
+  logsBloom: string,
+  transactionsRoot: string,
+  stateRoot: string,
+  miner: string,
+  difficulty: string,
+  totalDifficulty: string,
+  extraData: string,
+  size: string,
+  gasLimit: string,
+  gasUsed: string,
+  timestamp: string,
+  transactions: (string[] | Transaction[]),
+  uncles: string[]
+|}
+
+*/
