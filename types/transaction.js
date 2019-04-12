c3fac5cfb1818cb6218b89a69da97c5003ae628d
Mirek Rusin <mirek@me.com>
2019-04-12 07:51:03 +0200

    Adding transaction type.

---
+++ types/transaction.js
@@ -0,0 +1,22 @@
+// @flow
+
+/*::
+
+export type Transaction = {|
+  blockHash: string,
+  blockNumber: string,
+  from: string,
+  gas: string,
+  gasPrice: string,
+  hash: string,
+  input: string,
+  nonce: string,
+  to: string,
+  transactionIndex: ?number,
+  value: string,
+  v: string,
+  r: string,
+  s: string
+|}
+
+*/
