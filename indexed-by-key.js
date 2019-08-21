e5e0c1c0b640b5e618c767397d6f5d85633c67ca
Mirek Rusin <mirek@me.com>
2019-08-21 19:51:54 +0200

    Updating types of indexed-by-key.

--- indexed-by-key.js
+++ indexed-by-key.js
@@ -2,7 +2,7 @@
 
 /** @returns transposed entries from xs, indexed by `key`. Non-unique keys will overwrite previous values.
     Non-existing keys will be indexed under `undefined` key. */
-function indexedByKey/*:: <T> */(xs /*: T[] */, key /*: string */) /*: { [string]: T } */ {
+function indexedByKey/*:: <K: string, T: { [K]: string }> */(xs /*: T[] */, key /*: K */) /*: { [string]: T } */ {
   return xs.reduce((r, _) => {
     r[_[key]] = _
     return r
