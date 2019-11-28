767183df300a31a220eecca6c68eb7c26ba3a89f
Mirek Rusin <mirek@me.com>
2019-11-28 08:54:33 +0100

    Updating speculative nonce tests to work on real addresses.

--- test/speculative-nonce.test.js
+++ test/speculative-nonce.test.js
@@ -9,11 +9,11 @@ test('speculative nonce', async () => {
   const speculativeNonce = new SpeculativeNonce({
     nonceOfAddress: async () => 7
   })
-  await expect(speculativeNonce.next('0x1')).resolves.toEqual(7)
-  await expect(speculativeNonce.next('0x1')).resolves.toEqual(8)
-  await expect(speculativeNonce.next('0x1')).resolves.toEqual(9)
-  await expect(speculativeNonce.next('0x2')).resolves.toEqual(7)
+  await expect(speculativeNonce.next('0xD1E8bAACA71FeBF698fEaA1e2FACdCcC111D8c74')).resolves.toEqual(7)
+  await expect(speculativeNonce.next('0xD1E8bAACA71FeBF698fEaA1e2FACdCcC111D8c74')).resolves.toEqual(8)
+  await expect(speculativeNonce.next('0xD1E8bAACA71FeBF698fEaA1e2FACdCcC111D8c74')).resolves.toEqual(9)
+  await expect(speculativeNonce.next('0x328319440F9FBEedCFFd41cA54A47714ac79BD55')).resolves.toEqual(7)
   await sleep((12 + 1) * 1000)
-  await expect(speculativeNonce.next('0x1')).resolves.toEqual(7)
-  await expect(speculativeNonce.next('0x2')).resolves.toEqual(7)
+  await expect(speculativeNonce.next('0xD1E8bAACA71FeBF698fEaA1e2FACdCcC111D8c74')).resolves.toEqual(7)
+  await expect(speculativeNonce.next('0x328319440F9FBEedCFFd41cA54A47714ac79BD55')).resolves.toEqual(7)
 })
