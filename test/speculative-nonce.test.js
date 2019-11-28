d80bdd7d06432f1201da611f9214ff1881f5b3df
Mirek Rusin <mirek@me.com>
2019-11-28 08:36:17 +0100

    Adding tests around speculative nonce.

---
+++ test/speculative-nonce.test.js
@@ -0,0 +1,19 @@
+// @flow
+
+const SpeculativeNonce = require('../speculative-nonce')
+const sleep = require('../sleep')
+
+jest.setTimeout(60 * 1000)
+
+test('speculative nonce', async () => {
+  const speculativeNonce = new SpeculativeNonce({
+    nonceOfAddress: async () => 7
+  })
+  await expect(speculativeNonce.next('0x1')).resolves.toEqual(7)
+  await expect(speculativeNonce.next('0x1')).resolves.toEqual(8)
+  await expect(speculativeNonce.next('0x1')).resolves.toEqual(9)
+  await expect(speculativeNonce.next('0x2')).resolves.toEqual(7)
+  await sleep((12 + 1) * 1000)
+  await expect(speculativeNonce.next('0x1')).resolves.toEqual(7)
+  await expect(speculativeNonce.next('0x2')).resolves.toEqual(7)
+})
