f5efe12dc2bffde84f5c210d6cc5ac0f69d50941
Mirek Rusin <mirek@me.com>
2019-12-09 17:46:49 +0100

    Updating buffer-of-hex/hex0x helpers to work correctly on odd strings.

--- buffer-of-hex.js
+++ buffer-of-hex.js
@@ -1,13 +1,16 @@
 // @flow
 
+const { from: bufferOf } = Buffer
 const { inspect } = require('util')
+const evenZeroPaddedOf = require('./even-zero-padded-of')
 const isHex = require('./is-hex')
 
+/** @returns buffer representation of provided hex string. */
 function bufferOfHex(value /*: string */) {
   if (!isHex(value)) {
     throw new TypeError(`Expected hex string, got ${inspect(value)}.`)
   }
-  return Buffer.from(value, 'hex')
+  return bufferOf(evenZeroPaddedOf(value), 'hex')
 }
 
 module.exports = bufferOfHex
--- buffer-of-hex0x.js
+++ buffer-of-hex0x.js
@@ -1,13 +1,16 @@
 // @flow
 
+const { from: bufferOf } = Buffer
 const { inspect } = require('util')
+const evenZeroPaddedOf = require('./even-zero-padded-of')
 const isHex0x = require('./is-hex0x')
 
-function bufferOfHex0x(value /*: string */) {
+/** @returns buffer representation of provided hex0x string. */
+function bufferOfHex0x(value /*: string */) /*: Buffer */ {
   if (!isHex0x(value)) {
     throw new TypeError(`Expected hex0x string, got ${inspect(value)}.`)
   }
-  return Buffer.from(value.slice(2), 'hex')
+  return bufferOf(evenZeroPaddedOf(value.slice(2)), 'hex')
 }
 
 module.exports = bufferOfHex0x
