8ed9d780839e5b36a9a93bccea61b9167d1f18a9
Mirek Rusin <mirek@me.com>
2019-12-09 17:47:08 +0100

    Using padding helper in hex-of-number.

--- hex-of-number.js
+++ hex-of-number.js
@@ -2,16 +2,14 @@
 
 const { inspect } = require('util')
 const isSafeUnsigned = require('./is-safe-unsigned')
+const evenZeroPaddedOf = require('./even-zero-padded-of')
 
 /** @returns even/byte-padded, hex string representation of a safe, unsigned number. */
 function hexOfNumber(value /*: number */) /*: string */ {
   if (!isSafeUnsigned(value)) {
     throw new TypeError(`Expected safe, unsigned number, got ${inspect(value)}.`)
   }
-  const hex = value.toString(16)
-  return hex.length % 2 === 0 ?
-    hex :
-    '0' + hex
+  return evenZeroPaddedOf(value.toString(16))
 }
 
 module.exports = hexOfNumber
