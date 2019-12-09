5713bd549f9eb6f0d54acc0a8b2fec89069672e0
Mirek Rusin <mirek@me.com>
2019-12-09 17:54:55 +0100

    Renaming hex-of-number to hex-of-unsigned.

--- hex-of-unsigned.js
+++ hex-of-unsigned.js
@@ -5,11 +5,11 @@ const isSafeUnsigned = require('./is-safe-unsigned')
 const evenZeroPaddedOf = require('./even-zero-padded-of')
 
 /** @returns even/byte-padded, hex string representation of a safe, unsigned number. */
-function hexOfNumber(value /*: number */) /*: string */ {
+function hexOfUnsigned(value /*: number */) /*: string */ {
   if (!isSafeUnsigned(value)) {
     throw new TypeError(`Expected safe, unsigned number, got ${inspect(value)}.`)
   }
   return evenZeroPaddedOf(value.toString(16))
 }
 
-module.exports = hexOfNumber
+module.exports = hexOfUnsigned
