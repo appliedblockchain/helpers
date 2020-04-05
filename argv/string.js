afa816063cf15b757933a007879e356ebf4ab2df
Mirek Rusin <mirek@me.com>
2020-04-05 22:14:15 +0200

    Thow in args/string if default not provided and arg is not present.

--- argv/string.js
+++ argv/string.js
@@ -1,11 +1,16 @@
 // @flow
 
+const { inspect } = require('util')
+
 const string /*: (string, default_?: string, args?: string[]) => string */ =
-  (name, default_ = '', args = process.argv) => {
+  (name, default_, args = process.argv) => {
     for (let i = 0; i < args.length - 1; i++) {
       if (args[i] === name) {
         return args[i + 1]
       }
+    }
+    if (typeof default_ !== 'string') {
+      throw new TypeError(`Expected ${inspect(name)} arg.`)
     }
     return default_
   }
