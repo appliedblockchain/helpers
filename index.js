e81c58cad235932d2d1985010729f71b8d7fdb5a
Mirek Rusin <mirek@me.com>
2020-06-03 17:16:02 +0200

    Exporting helpers in index.

--- index.js
+++ index.js
@@ -1,1 +1,249 @@
 // @flow
+
+const addressOfPrivateKey = require('./address-of-private-key')
+const addressOfPublicKey = require('./address-of-public-key')
+const arrayOf = require('./array-of')
+const blockHashOf = require('./block-hash-of')
+const booleanOfString = require('./boolean-of-string')
+const bufferOfHexOrHex0x = require('./buffer-of-hex-or-hex0x')
+const bufferOfHex = require('./buffer-of-hex')
+const bufferOfHex0x = require('./buffer-of-hex0x')
+const bufferOfUnsignedBigInt = require('./buffer-of-unsigned-big-int')
+const bufferOfUnsigned = require('./buffer-of-unsigned')
+const buffer32OfNumber = require('./buffer32-of-number')
+const catchOf = require('./catch-of')
+const checksumAddressOfAddress = require('./checksum-address-of-address')
+const checksumAddressOfBuffer = require('./checksum-address-of-buffer')
+const checksumAddressOfPrivateKey = require('./checksum-address-of-private-key')
+const checksumAddressOfPublicKey = require('./checksum-address-of-public-key')
+const cryptoScrypt = require('./crypto-scrypt')
+const defaultAgentOfUrl = require('./default-agent-of-url')
+const defaultCmp = require('./default-cmp')
+const defaultHttpAgent = require('./default-http-agent')
+const defaultHttpsAgent = require('./default-https-agent')
+const eachPromise = require('./each-promise')
+const errOf = require('./err-of')
+const Err = require('./err')
+const evenZeroPaddedOf = require('./even-zero-padded-of')
+const eventuallyTrue = require('./eventually-true')
+const every = require('./every')
+const expirySet = require('./expiry-set')
+const fibonacciOf = require('./fibonacci-of')
+const firstOf = require('./first-of')
+const get = require('./get')
+const hasFunction = require('./has-function')
+const hasKeys = require('./has-keys')
+const has = require('./has')
+const hashOfSignature = require('./hash-of-signature')
+const heightOf = require('./height-of')
+const hexOfBuffer = require('./hex-of-buffer')
+const hexOfByte = require('./hex-of-byte')
+const hexOfHex0x = require('./hex-of-hex0x')
+const hexOfUnsigned = require('./hex-of-unsigned')
+const hex0xOfBuffer = require('./hex0x-of-buffer')
+const hex0xOfHex = require('./hex0x-of-hex')
+const hex0xOfUnsigned = require('./hex0x-of-unsigned')
+const inc = require('./inc')
+const indexedByKey = require('./indexed-by-key')
+const integerOfString = require('./integer-of-string')
+const isAddress = require('./is-address')
+const isBlockNumberOrTag = require('./is-block-number-or-tag')
+const isBlockQueryTag = require('./is-block-query-tag')
+const isBoolean = require('./is-boolean')
+const isDefined = require('./is-defined')
+const isFinite = require('./is-finite')
+const isHex = require('./is-hex')
+const isHex0x = require('./is-hex0x')
+const isLikeDateString = require('./is-like-date-string')
+const isNil = require('./is-nil')
+const isNull = require('./is-null')
+const isObject = require('./is-object')
+const isPending = require('./is-pending')
+const isPlainObjectShallow = require('./is-plain-object-shallow')
+const isPromise = require('./is-promise')
+const isSafeNonNegative = require('./is-safe-non-negative')
+const isSafePositive = require('./is-safe-positive')
+const isSafeUnsigned = require('./is-safe-unsigned')
+const isSettled = require('./is-settled')
+const isSorted = require('./is-sorted')
+const isString = require('./is-string')
+const isUint32 = require('./is-uint32')
+const isUndefined = require('./is-undefined')
+const isUnique = require('./is-unique')
+const jsonOfFile = require('./json-of-file')
+const jsonOfUrl = require('./json-of-url')
+const jsonRpcError = require('./json-rpc-error')
+const keccak256F1600 = require('./keccak256-f1600')
+const keccak256OfBuffer = require('./keccak256-of-buffer')
+const keysOf = require('./keys-of')
+const keythereumOf = require('./keythereum-of')
+const kindOfJsonRpc20 = require('./kind-of-json-rpc-2.0')
+const lastOf = require('./last-of')
+const latestBlockOfUrl = require('./latest-block-of-url')
+const lineOfBuffer = require('./line-of-buffer')
+const linesOfBuffer = require('./lines-of-buffer')
+const logOf = require('./log-of')
+const majorityOfArray = require('./majority-of-array')
+const majorityOfNumber = require('./majority-of-number')
+const makeRetrace = require('./make-retrace')
+const mapPath = require('./map-path')
+const mapValues = require('./map-values')
+const maybeLastOf = require('./maybe-last-of')
+const meta = require('./meta')
+const noop = require('./noop')
+const numberOfHex0x = require('./number-of-hex0x')
+const objectOfStrings = require('./object-of-strings')
+const onceIf = require('./once-if')
+const once = require('./once')
+const pbkdf2Of = require('./pbkdf2-of')
+const postJsonRpc = require('./post-json-rpc')
+const postJson = require('./post-json')
+const publicKeyOfPrivateKey = require('./public-key-of-private-key')
+const racePromises = require('./race-promises')
+const randomOfNormalDistribution = require('./random-of-normal-distribution')
+const randomUint32 = require('./random-uint32')
+const rejectTimeout = require('./reject-timeout')
+const requestOfUrl = require('./request-of-url')
+const resultOfPredicate = require('./result-of-predicate')
+const retry = require('./retry')
+const scryptOf = require('./scrypt-of')
+const serialQueueOf = require('./serial-queue-of')
+const shSync = require('./sh-sync')
+const shuffle = require('./shuffle')
+const sleep = require('./sleep')
+const sortedDifference = require('./sorted-difference')
+const sortedIntersection = require('./sorted-intersection')
+const sortedKeys = require('./sorted-keys')
+const speculativeNonce = require('./speculative-nonce')
+const spread = require('./spread')
+const stopOfMonitor = require('./stop-of-monitor')
+const timeoutOf = require('./timeout-of')
+const uuidStringOfBuffer = require('./uuid-string-of-buffer')
+const uuidV4 = require('./uuid-v4')
+const uuid = require('./uuid')
+const webSocketReadyState = require('./web-socket-ready-state')
+
+module.exports = {
+  addressOfPrivateKey,
+  addressOfPublicKey,
+  arrayOf,
+  blockHashOf,
+  booleanOfString,
+  bufferOfHexOrHex0x,
+  bufferOfHex,
+  bufferOfHex0x,
+  bufferOfUnsignedBigInt,
+  bufferOfUnsigned,
+  buffer32OfNumber,
+  catchOf,
+  checksumAddressOfAddress,
+  checksumAddressOfBuffer,
+  checksumAddressOfPrivateKey,
+  checksumAddressOfPublicKey,
+  cryptoScrypt,
+  defaultAgentOfUrl,
+  defaultCmp,
+  defaultHttpAgent,
+  defaultHttpsAgent,
+  eachPromise,
+  errOf,
+  Err,
+  evenZeroPaddedOf,
+  eventuallyTrue,
+  every,
+  expirySet,
+  fibonacciOf,
+  firstOf,
+  get,
+  hasFunction,
+  hasKeys,
+  has,
+  hashOfSignature,
+  heightOf,
+  hexOfBuffer,
+  hexOfByte,
+  hexOfHex0x,
+  hexOfUnsigned,
+  hex0xOfBuffer,
+  hex0xOfHex,
+  hex0xOfUnsigned,
+  inc,
+  indexedByKey,
+  integerOfString,
+  isAddress,
+  isBlockNumberOrTag,
+  isBlockQueryTag,
+  isBoolean,
+  isDefined,
+  isFinite,
+  isHex,
+  isHex0x,
+  isLikeDateString,
+  isNil,
+  isNull,
+  isObject,
+  isPending,
+  isPlainObjectShallow,
+  isPromise,
+  isSafeNonNegative,
+  isSafePositive,
+  isSafeUnsigned,
+  isSettled,
+  isSorted,
+  isString,
+  isUint32,
+  isUndefined,
+  isUnique,
+  jsonOfFile,
+  jsonOfUrl,
+  jsonRpcError,
+  keccak256F1600,
+  keccak256OfBuffer,
+  keysOf,
+  keythereumOf,
+  kindOfJsonRpc20,
+  lastOf,
+  latestBlockOfUrl,
+  lineOfBuffer,
+  linesOfBuffer,
+  logOf,
+  majorityOfArray,
+  majorityOfNumber,
+  makeRetrace,
+  mapPath,
+  mapValues,
+  maybeLastOf,
+  meta,
+  noop,
+  numberOfHex0x,
+  objectOfStrings,
+  onceIf,
+  once,
+  pbkdf2Of,
+  postJsonRpc,
+  postJson,
+  publicKeyOfPrivateKey,
+  racePromises,
+  randomOfNormalDistribution,
+  randomUint32,
+  rejectTimeout,
+  requestOfUrl,
+  resultOfPredicate,
+  retry,
+  scryptOf,
+  serialQueueOf,
+  shSync,
+  shuffle,
+  sleep,
+  sortedDifference,
+  sortedIntersection,
+  sortedKeys,
+  speculativeNonce,
+  spread,
+  stopOfMonitor,
+  timeoutOf,
+  uuidStringOfBuffer,
+  uuidV4,
+  uuid,
+  webSocketReadyState
+}
