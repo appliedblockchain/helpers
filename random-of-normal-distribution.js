726617f699547488cdaebca025ea37c7bd2ee380
Mirek Rusin <mirek@me.com>
2020-06-03 17:06:35 +0200

    Adding random-of-normal-distribution.

---
+++ random-of-normal-distribution.js
@@ -0,0 +1,27 @@
+// @flow
+
+const { EPSILON: epsilon } = Number
+const { random, sqrt, PI: pi, log, sin, cos } = Math
+
+const tau = pi * 2
+
+let flip = false
+let z1 = 0
+
+/** @returns normally distributed random number(s) with `mean` and `standardDeviation`. */
+const randomOfNormalDistribution /*: (mean?: number, standardDeviation?: number) => number */ =
+  (mean = 0, standardDeviation = 1) => {
+    if (!(flip = !flip)) {
+      return (z1 * standardDeviation) + mean
+    }
+    let u1
+    do {
+      u1 = random()
+    } while (u1 <= epsilon)
+    const u2 = random()
+    const z0 = sqrt(-2.0 * log(u1)) * cos(tau * u2)
+    z1 = sqrt(-2.0 * log(u1)) * sin(tau * u2)
+    return (z0 * standardDeviation) + mean
+  }
+
+module.exports = randomOfNormalDistribution
